a9f1274d6ddc9e84401848274cf4b5332f5a66ca
Mahmoud Saad <62609750+Mahmoud-Saad92@users.noreply.github.com>
2025-11-21 23:50:21 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# polar-edge-service
-API gateway and cross-cutting concerns
+# `Bazinga (BZA)` edge-service
+This application is part of the Polar Bookshop system that provides API gateway and cross-cutting concerns.
---
+++ src/test/java/com/bazinga/eg/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bazinga.eg.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
